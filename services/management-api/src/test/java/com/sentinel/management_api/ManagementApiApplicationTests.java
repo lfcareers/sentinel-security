fe6d91d9294e7ee850d85fb6b63cc6a43cf3e32f
lfcareers <lfcareers@outlook.com>
2026-09-16 17:08:07 -0400

    Add authenticated agent telemetry ingestion

--- services/management-api/src/test/java/com/sentinel/management_api/ManagementApiApplicationTests.java
+++ services/management-api/src/test/java/com/sentinel/management_api/ManagementApiApplicationTests.java
@@ -3,11 +3,15 @@ package com.sentinel.management_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.flyway.enabled=false",
+        "sentinel.kafka.enabled=false",
+        "sentinel.ingest.key=test-ingest-key",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class ManagementApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
